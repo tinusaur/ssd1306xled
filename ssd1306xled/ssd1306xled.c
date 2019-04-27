653fd7fb3c3e8f06ce6e779aeb3180385f7c9e80
boyanov <none@none>
2019-04-27 20:19:21 +0300

    Updated ssd1306_init_sequence, fixes and more, compatibility with SSD1315.

--- ssd1306xled/ssd1306xled.c
+++ ssd1306xled/ssd1306xled.c
@@ -14,6 +14,11 @@
  */
 
 // ============================================================================
+// ACKNOWLEDGEMENTS:
+// - Some code and ideas initially based on "IIC_wtihout_ACK" 
+//   by http://www.14blog.com/archives/1358 (defunct)
+// - Init sequence used info from Adafruit_SSD1306.cpp init code.
+// ============================================================================
 
 #include <stdlib.h>
 #include <avr/io.h>
@@ -31,10 +36,32 @@ void ssd1306_stop(void);	// Finish transmission
 // ----------------------------------------------------------------------------
 
 const uint8_t ssd1306_init_sequence [] PROGMEM = {	// Initialization Sequence
+
+	0xAE,			// Set Display ON/OFF - AE=OFF, AF=ON
+	0xD5, 0xF0,		// Set display clock divide ratio/oscillator frequency, set divide ratio
+	0xA8, 0x3F,		// Set multiplex ratio (1 to 64) ... (height - 1)
+	0xD3, 0x00,		// Set display offset. 00 = no offset
+	0x40 | 0x00,	// Set start line address, at 0.
+	0x8D, 0x14,		// Charge Pump Setting, 14h = Enable Charge Pump
+	0x20, 0x00,		// Set Memory Addressing Mode - 00=Horizontal, 01=Vertical, 10=Page, 11=Invalid
+	0xA0 | 0x01,	// Set Segment Re-map
+	0xC8,			// Set COM Output Scan Direction
+	0xDA, 0x12,		// Set COM Pins Hardware Configuration - 128x32:0x02, 128x64:0x12
+	0x81, 0x3F,		// Set contrast control register
+	0xD9, 0x22,		// Set pre-charge period (0x22 or 0xF1)
+	0xDB, 0x20,		// Set Vcomh Deselect Level - 0x00: 0.65 x VCC, 0x20: 0.77 x VCC (RESET), 0x30: 0.83 x VCC
+	0xA4,			// Entire Display ON (resume) - output RAM to display
+	0xA6,			// Set Normal/Inverse Display mode. A6=Normal; A7=Inverse
+	0x2E,			// Deactivate Scroll command
+	0xAF,			// Set Display ON/OFF - AE=OFF, AF=ON
+	//
+	0x22, 0x00, 0x3f,	// Set Page Address (start,end)
+	0x21, 0x00,	0x7f,	// Set Column Address (start,end)
+	//
+	// 0xD6, 0x01,		// Set Zoom In, 0=disabled, 1=enabled
+/*
 	0xAE,			// Display OFF (sleep mode)
-	0x20, 0x00,		// Set Memory Addressing Mode
-	//				// 00=Horizontal Addressing Mode; 01=Vertical Addressing Mode;
-	//				// 10=Page Addressing Mode (RESET); 11=Invalid
+	0x20, 0x00,		// Set Memory Addressing Mode - 00=Horizontal, 01=Vertical, , 10=Page, 11=Invalid
 	0xB0,			// Set Page Start Address for Page Addressing Mode, 0-7
 	0xC8,			// Set COM Output Scan Direction
 	0x00,			// ---set low column address
@@ -44,24 +71,21 @@ const uint8_t ssd1306_init_sequence [] PROGMEM = {	// Initialization Sequence
 	0xA1,			// Set Segment Re-map. A0=address mapped; A1=address 127 mapped.
 	0xA6,			// Set display mode. A6=Normal; A7=Inverse
 	0xA8, 0x3F,		// Set multiplex ratio(1 to 64)
-	0xA4,			// Output RAM to Display
-	//				// 0xA4=Output follows RAM content; 0xA5,Output ignores RAM content
+	0xA4,			// Output RAM to Display - 0xA4=Output follows RAM content; 0xA5,Output ignores RAM content
 	0xD3, 0x00,		// Set display offset. 00 = no offset
-	0xD5,			// --set display clock divide ratio/oscillator frequency
-	0xF0,			// --set divide ratio
+	0xD5, 0xF0,		// --set display clock divide ratio/oscillator frequency
 	0xD9, 0x22,		// Set pre-charge period
 	0xDA, 0x12,		// Set com pins hardware configuration
-	0xDB,			// --set vcomh
-	0x20,			// 0x20,0.77xVcc
-	0x8D, 0x14,		// Set DC-DC enable
-	0xAF			// Display ON in normal mode
+	0xDB, 0x20,		// --set vcomh, 0x20,0.77xVcc
+	0x8D, 0x14,		// Charge Pump Setting, 14h = Enable Charge Pump
+	0xAF,			// Display ON in normal mode
+*/
 };
 
 // ============================================================================
 
-// TODO: These functions are separate sub-library for handling I2C simplified output.
+// NOTE: These functions are separate sub-library for handling I2C simplified output.
 // NAME: I2CSW - I2C Simple Writer.
-// Some code based on "IIC_wtihout_ACK" by http://www.14blog.com/archives/1358 (defunct)
 // Convenience definitions for manipulating PORTB pins
 // NOTE: These definitions are used only internally by the I2CSW library
 #define I2CSW_HIGH(PORT) PORTB |= (1 << PORT)
